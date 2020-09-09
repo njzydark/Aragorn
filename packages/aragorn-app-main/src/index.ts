d2266032ff646d58263e2dffa04dac0f298a8ed2
njzy <njzydark@gmail.com>
2020-09-10 00:01:53 +0800

    refactor: use requestSingleInstanceLock

--- packages/aragorn-app-main/src/index.ts
+++ packages/aragorn-app-main/src/index.ts
@@ -8,6 +8,58 @@ let mainWindow: BrowserWindow;
 
 const isDev = process.env.NODE_ENV === 'development';
 
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', () => {
+    if (mainWindow && BrowserWindow.getAllWindows().length !== 0) {
+      if (mainWindow.isMinimized()) {
+        mainWindow.restore();
+      }
+      mainWindow.focus();
+    }
+  });
+
+  app.on('ready', () => {
+    tray = new Tray(path.resolve(__dirname, '../assets/trayIconTemplate.png'));
+
+    tray.addListener('click', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        mainWindow = createWindow();
+        Ipc.win = mainWindow;
+      } else {
+        mainWindow.show();
+      }
+    });
+
+    // 托盘拖拽上传
+    tray.addListener('drop-files', (_, files) => {
+      UploaderManager.getInstance().upload(files);
+    });
+
+    mainWindow = createWindow();
+    Ipc.win = mainWindow;
+    Ipc.getInstance();
+  });
+
+  app.on('window-all-closed', function () {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') app.quit();
+  });
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      mainWindow = createWindow();
+      Ipc.win = mainWindow;
+    }
+  });
+}
+
 function createWindow(): BrowserWindow {
   const window = new BrowserWindow({
     width: 950,
@@ -27,42 +79,3 @@ function createWindow(): BrowserWindow {
 
   return window;
 }
-
-app.on('ready', () => {
-  tray = new Tray(path.resolve(__dirname, '../assets/trayIconTemplate.png'));
-
-  tray.addListener('click', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      mainWindow = createWindow();
-      Ipc.win = mainWindow;
-    } else {
-      mainWindow.show();
-    }
-  });
-
-  // 托盘拖拽上传
-  tray.addListener('drop-files', (_, files) => {
-    new UploaderManager().upload(files);
-  });
-
-  mainWindow = createWindow();
-  Ipc.win = mainWindow;
-  Ipc.getInstance().init();
-});
-
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit();
-});
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    mainWindow = createWindow();
-    Ipc.win = mainWindow;
-  }
-});
-
-app.allowRendererProcessReuse = true;
