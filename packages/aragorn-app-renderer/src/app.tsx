693859d84156d3d15df8a3d6496584f9486262e3
njzy <njzydark@gmail.com>
2020-10-07 18:36:22 +0800

    feat(renderer): add route transition

--- packages/aragorn-app-renderer/src/app.tsx
+++ packages/aragorn-app-renderer/src/app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 import { routes } from './routes';
 import { SideBar } from '@renderer/components/SideBar';
 import { WindowButton } from '@renderer/components/WindowButton';
@@ -23,12 +24,29 @@ const App = () => {
     <HashRouter>
       {platform === 'win32' && <WindowButton />}
       <SideBar className={platform === 'win32' ? 'app-win32-wrapper' : ''} routes={routes} />
-      <div className={platform === 'win32' ? 'app-main-content-wrapper app-win32-wrapper' : 'app-main-content-wrapper'}>
-        <Switch>
-          {routes.map(
-            route => route.path && <Route key={route.path} path={route.path} component={route.component} exact />
-          )}
-        </Switch>
+      <div className="page-container">
+        {routes.map(
+          route =>
+            route.path && (
+              <Route key={route.path} path={route.path} exact>
+                {({ match }) => (
+                  <CSSTransition in={match !== null} timeout={300} classNames="page" unmountOnExit>
+                    <div className="page">
+                      <div
+                        className={
+                          platform === 'win32'
+                            ? 'app-main-content-wrapper app-win32-wrapper'
+                            : 'app-main-content-wrapper'
+                        }
+                      >
+                        {route.component && <route.component />}{' '}
+                      </div>
+                    </div>
+                  </CSSTransition>
+                )}
+              </Route>
+            )
+        )}
       </div>
     </HashRouter>
   );
